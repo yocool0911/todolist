c08fdd404354b6bfe9ae74490ce6535405b25675
yozu <yocool0911@gmail.com>
2020-11-20 17:26:16 +0800

    基本功能加上 只剩下樣式調整 下方未完成式項顯示

--- js/main.js
+++ js/main.js
@@ -4,6 +4,47 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
-    }
+      newtodo:"",  
+      todos: [],
+      visibility: "all",
+    },
+    methods: {
+         addtodo(){
+             let newtodo = this.newtodo;
+             var timestamp = Math.floor(Date.now());
+             this.todos.push({
+                 id: timestamp ,
+                 title: newtodo,
+                 completed: false
+             })
+             this.newtodo = "";
+         },
+         removetodo(todo){
+            let newIndex = "";
+            let vm = this;
+            vm.todos.forEach( (item,key) => {
+                 if(item.id == item.id){
+                    newIndex = key;
+                 }
+                 vm.todos.splice(newIndex,1);
+            });
+         }
+    },
+    computed: {
+         filtertodo(){
+             if(this.visibility  == "all"){
+                return this.todos;
+             }
+             else if(this.visibility === "ing"){
+                  alert('有');
+                let newtodo = [];
+                  this.todos.forEach( (item) => {
+                      if(!item.completed){
+                        newtodo.push(item);
+                      }
+                  })
+                  return newtodo
+             }
+         }
+    },
   })
